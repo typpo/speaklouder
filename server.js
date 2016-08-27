1d08d0c5b12651d1d80780aeed23bbbf578b10fd
Ian Webster <ianw_git@ianww.com>
2016-08-27 11:31:01 -0700

    disable view cache

--- server.js
+++ server.js
@@ -25,13 +25,13 @@ require('./config/passport');
 
 var app = express();
 
-
 mongoose.connect(process.env.MONGODB);
 mongoose.connection.on('error', function() {
   console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
   process.exit(1);
 });
 // view engine setup
+app.disable('view cache');
 nunjucks.configure('views', {
   autoescape: true,
   express: app
